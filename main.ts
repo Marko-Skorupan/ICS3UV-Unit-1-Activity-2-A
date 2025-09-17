49e57cd7a24433ac526a1361575051bb4a91f8a7
default <marko.skorupan@stu.ocsb.ca>
2025-09-17 23:29:27 +0000

    Updated main.ts with my name and current date

---
+++ main.ts
@@ -0,0 +1,11 @@
+/**
+ * @author Marko skorupan
+ * @version 1.0.0
+ * @date 2025-09-17
+ * @fileoverview This is a traditional "Hello, World!" program.
+ */
+
+// print out the greeting
+console.log("Hello, World!")
+
+console.log("\nDone")
