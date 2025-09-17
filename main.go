d388704fb942a6caf49b3904b5388157c28fddc1
default <marko.skorupan@stu.ocsb.ca>
2025-09-17 23:38:31 +0000

    Updated main.ts with my name and current date

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
+/*
+*Author: Marko Skorupan
+*Version: 1.0.0
+*date: 2025-09-17
+*This is a traditional "Hello, World!" program.
+*/
+
 package main
 
 import "fmt"
 
 func main() {
+	// print out the greeting
 	fmt.Println("Hello, World!")
+
+	fmt.Println("\nDone.")
 }
